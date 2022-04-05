de3f8f5921e6583dc5ff6bceca52750e68f93cab
Niall-Sheerin <60427199+Niall-Sheerin@users.noreply.github.com>
2022-04-05 12:35:59 +0100

    json data set

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port =  3000 
 
+let drivers = [];
+
+app.post('/drivers', (req, res) => {
+    const driver = req.body;
+    
+    const driverNumber = driver.length ;
+    
+    drivers.push(driver);
+
+    res.location(`/drivers/${driverNumber}`);
+    res.status(201);
+    res.json(driver);
+});
+
 app.get('/', (req, res) => res.send('Hello world from Niall'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
