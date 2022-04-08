6ed2cf70efe9de68b319eb95494e842234d6f763
Niall-Sheerin <60427199+Niall-Sheerin@users.noreply.github.com>
2022-04-08 16:19:17 +0100

    component created

--- f1OSDapp2022/src/app/driver.ts
+++ f1OSDapp2022/src/app/driver.ts
@@ -1,2 +1,9 @@
 export interface Driver {
+    _id: string,
+    name : string,
+    number: number,
+    constructor: string,
+    age: number,
+    origin : { nationality : string },
+    tags: [string];
 }
